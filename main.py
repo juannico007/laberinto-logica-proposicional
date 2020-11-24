c5c9de1b62eefd795c0a770a299467b7801aeb3c
Nico Quintero <juan24092002@gmail.com>
2020-11-24 09:06:13 -0500

    guarda reglas en archivo.json a parte

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import FNC as ts
 import maze as mz
 import visualizacion as vs
 import DPLL as dp
+import guardar_reglas as gr
 
 letrasProposicionales = [chr(x) for x in range(256, 2706)]
 interp_verdaderas = {}
@@ -13,13 +14,13 @@ Ncolumnas = 7
 Nturnos = 49
 letras_muros = []
 letras_turnos = []
-muros = []
 
 #Establece el inicio y el final dado por el usuario
 inicio, final = fn.inicio_final(Nfilas, Ncolumnas)
+archivo = str(inicio) + "-" + str(final)
 
 #Crea el laberinto conectado
-walls = mz.create_maze(inicio, final, Nfilas, Ncolumnas)
+muros = mz.create_maze(inicio, final, Nfilas, Ncolumnas)
 
 print("Generando letras")
 
@@ -30,19 +31,6 @@ for i in range(Nfilas):
         cod = chr(n + 256)
         letras_muros.append(cod)
 
-#Asigna valores de verdad a los muros con el laberinto hecho
-for(i, j) in walls:
-    n = fn.codifica(i, j, Nfilas, Ncolumnas)
-    cod = chr(n + 256)
-    muros.append(cod)
-for i in muros:
-        interp_verdaderas[i] = 1
-for i in range(Nfilas):
-    for j in range(Ncolumnas):
-        if (i, j) not in walls:
-            n = fn.codifica(i, j, Nfilas, Ncolumnas)
-            cod = chr(n + 256)
-            interp_verdaderas[cod] = 0
 
 #Letras correspondientes a la presencia del agente en una casilla en un turno
 for i in range(Nturnos):
@@ -55,9 +43,13 @@ for i in range(Nturnos):
 print("Generando reglas y formula")
 formula = ""
 
+#Condiciones iniciales
+cond_iniciales = fn.cond_inicial(muros, Nfilas, Ncolumnas)
+formula += cond_iniciales
+
 #Regla 1
 formula_1 = fn.regla_1(inicio, final, Nfilas, Ncolumnas, Nturnos)
-formula += formula_1
+formula += formula_1 + "Y"
 
 #Regla 2
 formula_2 = fn.regla_2(Nfilas, Ncolumnas, Nturnos)
@@ -75,17 +67,8 @@ formula += formula_4 + "Y"
 formula_5 = fn.regla_5(final, Nfilas, Ncolumnas, Nturnos)
 formula += formula_5 + "Y"
 
-#Regla 6
-formula_6 = fn.regla_6(final, Nfilas, Ncolumnas, Nturnos)
-formula += formula_6 + "Y"
-
-print("Formula hecha")
-print("Ejecutando transformacion de Tseitin")
-x = ts.Tseitin(fn.String2Tree(formula).inorder(), letrasProposicionales)
-x = ts.formaClausal(x)
-print("Transformacion de Tseitin hecha")
-print("Forma normal conjuntiva hallada")
-
+gr.guardar_polaca(formula, archivo, letrasProposicionales)
+'''
 print("Buscando interpretación que satisfaga el problema")
 I = dp.DPLL(x, interp_verdaderas)
 interp_verdaderas = {}
@@ -93,7 +76,7 @@ print("Interpretación hallada")
 print("Eliminando letras adicionales")
 for i in I:
     if i in letras_muros:
-        m = ord(i) - 256 - (Nfilas * Ncolumnas)
+        m = ord(i) - 256
         interp_verdaderas[m] = I[i]
     elif i in letras_turnos:
         m = ord(i) - 256
@@ -102,4 +85,5 @@ print("Letras adicionales eliminadas")
 for i in range(Nfilas * Ncolumnas * (Nturnos + 1)):
     if i not in interp_verdaderas:
         interp_verdaderas[i] = 0
-vs.dibujar_tablero(interp_verdaderas, Nfilas, Ncolumnas, Nturnos, 122)
+
+vs.dibujar_tablero(interp_verdaderas, Nfilas, Ncolumnas, Nturnos, 122)'''
